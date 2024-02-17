40ec1fbddde7c5cba8ce5e246ba55f18f1666a1b
Luis Amorim <luisfelipeamorim@hotmail.com>
2024-02-17 20:14:40 -0300

    initial

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install build-essential
+
+sudo apt install zsh
+
+chsh -s $(which zsh)
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+sudo apt update
+
+sudo apt install gh
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+
+sudo rm -rf /opt/nvim
+
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>  ~/.zshrc
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
+
+
